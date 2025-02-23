f6fb890a711e7a803dc698a91ee047f00a07718e
deokgoo <kkddgg1001@gmail.com>
2025-02-23 22:58:03 +0900

    generate sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,16 +4,17 @@ import siteMetadata from '@/data/siteMetadata';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl;
+  const cleanSlug = (slug) => slug.trim().replace(/[^\x20-\x7E]/g, '').replace(/\s+/g, '-').replace(/[^\w-]/g, '');
 
   const blogRoutes = allBlogs
     .filter((post) => !post.draft)
     .map((post) => ({
-      url: `${siteUrl}/${post.path}`,
-      lastModified: post.lastmod || post.date,
+      url: `${siteUrl}/${cleanSlug(post.path)}`,
+      lastModified: cleanSlug(post.lastmod || post.date),
     }));
 
   const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
-    url: `${siteUrl}/${route}`,
+    url: `${siteUrl}/${cleanSlug(route)}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
 
