ddc3e1e54e4f679b208d3b1df0b9ba9efad9bae1
deokgoo <kkddgg1001@gmail.com>
2026-01-02 13:59:25 +0900

    chore: 프로젝트 페이지 더미 데이터 및 안내 문구 제거
    - projectsData.ts에서 더미 프로젝트 2개 삭제
    - 프로젝트 페이지 안내 문구 제거
    - 실제 프로젝트 추가를 위한 준비 완료

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,22 +6,7 @@ interface Project {
 }
 
 const projectsData: Project[] = [
-  {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+  // 실제 프로젝트 데이터를 추가할 예정
 ]
 
 export default projectsData
